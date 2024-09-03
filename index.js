f068c556c138837b5acc2b5f3c3bf54f89fb7c14
Christian <91901457+chriixd@users.noreply.github.com>
2024-09-04 00:58:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,85 @@
 function addProgress() {
+   lastExp = expCount;
     if(currentProgress < quests[gameProgress].progress) {
         currentProgress++        
     } else{
         expCount += quests[gameProgress].exp;
+        moneyCount += quests[gameProgress].coins;
         currentProgress = 0;
         gameProgress++;
-
+        if(gameProgress == 8) {
+            isLastQuest = true;
+        } else {
+            isLastQuest = false;
+        }
+        if(isLastQuest) {
+            document.getElementById('quest-container').classList.add('hidden');
+            document.getElementById('quest-container').classList.remove('flex');
+            document.getElementById('last-completed-quest').classList.remove('hidden');
+            document.getElementById('last-completed-quest').classList.add('flex');
+        }
     }
     save_data();
-    update_html();
+    update_html(Math.floor(expCount / maxExp) != Math.floor(lastExp / maxExp));
 }
 
-function update_html() {
+function update_html(levelUp = false) {
     if(document.title != 'Title Screen') {
-        if (document.title == 'Home') {
-            document.getElementById('quest-name').innerHTML = quests[gameProgress].title;
-            document.getElementById('quest-description').innerHTML = quests[gameProgress].desc;
-            document.getElementById('reward-coins').innerHTML = '+' + quests[gameProgress].coins;
-            document.getElementById('reward-exp').innerHTML = '+' + quests[gameProgress].exp;
-            document.getElementById('quest-max-progress').innerHTML = quests[gameProgress].progress;
-            document.getElementById('quest-progress').innerHTML = currentProgress;
+        if (document.title == 'Home' && gameProgress < 8) {
+                document.getElementById('quest-name').innerHTML = quests[gameProgress].title;
+                document.getElementById('quest-description').innerHTML = quests[gameProgress].desc;
+                document.getElementById('reward-coins').innerHTML = '+' + quests[gameProgress].coins;
+                document.getElementById('reward-exp').innerHTML = '+' + quests[gameProgress].exp;
+                document.getElementById('quest-max-progress').innerHTML = quests[gameProgress].progress;
+                document.getElementById('quest-progress').innerHTML = currentProgress;
+            Array.from(document.getElementsByClassName('completed-quest')).forEach((element, index, arr) => {
+                if(index < gameProgress ) {
+                    document.getElementsByClassName('completed-quest')[index].classList.add('flex');
+                    document.getElementsByClassName('completed-quest')[index].classList.remove('hidden');
+                    document.getElementsByClassName('completed-quest')[index].getElementsByTagName('h1')[0].textContent = quests[index].title;
+                }
+            });
+            Array.from(document.getElementsByClassName('locked-quest')).forEach((element, index, arr) => {
+                if (index < gameProgress) {
+                    document.getElementsByClassName('locked-quest')[index].classList.remove('flex');
+                    document.getElementsByClassName('locked-quest')[index].classList.add('hidden');
+                }
+            });
         }
+        if(levelUp) {
+            document.getElementById('level-up-section').classList.add('flex');
+            document.getElementById('level-up-section').classList.remove('hidden');
+            document.getElementById('level-up-animation').play();
+            setTimeout(() => {
+                document.getElementById('level-up-section').classList.remove('flex');
+                document.getElementById('level-up-section').classList.add('hidden');
+            }, 2812);
+        }
+        document.getElementById('money-amount').innerHTML = moneyCount;
         document.getElementById('exp-level').innerHTML = Math.floor(expCount / maxExp) + 1;
         document.getElementById('exp-progress').style.width = (((expCount %maxExp) / maxExp) * 100) + '%';
+        if(gameCompleted) {
+            document.getElementById('quest-container').classList.add('hidden');
+            document.getElementById('quest-container').classList.remove('flex');
+            document.getElementById('last-completed-quest').classList.remove('hidden');
+            document.getElementById('last-completed-quest').classList.add('flex');
+        }
     }
+
 }
 
 function save_data(){
-    localStorage.setItem('gameProgress', gameProgress);
+    if(gameProgress < 8) {
+        localStorage.setItem('gameProgress', gameProgress);
+    } else {
+        localStorage.setItem('gameProgress', 7);
+        gameCompleted = true;
+        localStorage.setItem('gameCompleted', gameCompleted);
+        console.log("max reached", gameCompleted)
+    }
     localStorage.setItem('expCount', expCount);
+    localStorage.setItem('moneyCount', moneyCount);
+    localStorage.setItem('quest-progress', currentProgress);
 }
 
 function display_log(when){
@@ -36,13 +87,16 @@ function display_log(when){
     console.log("gameProgress : " + localStorage.getItem('gameProgress'));
     console.log(expCount);
     console.log("gameProgress : " + gameProgress);
+    console.log(gameCompleted);
 }
 
 function load_vars(){
      gameProgress = parseInt(localStorage.getItem('gameProgress'))  || 0;
-     currentProgress = parseInt(localStorage.getItem('currentProgress'))  || 0;
+     currentProgress = parseInt(localStorage.getItem('quest-progress'))  || 0;
      expCount = parseInt(localStorage.getItem('expCount')) || 0;
-     firstTime =  localStorage.getItem('firstTime') || true;
+     moneyCount = parseInt(localStorage.getItem('moneyCount')) || 100;
+     firstTime = localStorage.getItem('firstTime') || true;
+     gameCompleted = localStorage.getItem('gameCompleted') || false;
      maxExp = 100;
 }
 
@@ -60,7 +114,7 @@ function startGame() {
     window.location.href = "home.html";
 }
 
-var maxExp,gameProgress,currentProgress,expCount,firstTime;
+var maxExp,gameProgress,currentProgress,expCount,firstTime,moneyCount;
 load_vars()
 if(!firstTime){window.location.href = "home.html";}
 display_log("post-dichiarazione-variabili")
---
+++ vars.js
@@ -0,0 +1,59 @@
+    quests = [
+      {
+        "title": "Quest di Flosa",
+        "desc": "Descrizione quest di Flosa",
+        "exp": 50,
+        "coins": 100,
+        "progress": 1
+      },
+      {
+        "title": "Quest di Max & Marco",
+        "desc": "Descrizione quest di Max & Marco",
+        "exp": 25,
+        "coins": 50,
+        "progress": 2
+      },
+      {
+        "title": "Quest di Elisa",
+        "desc": "Descrizione quest di Elisa",
+        "exp": 50,
+        "coins": 25,
+        "progress": 1
+      },
+      {
+        "title": "Quest di Sierra & Matt",
+        "desc": "Descrizione quest di Sierra & Matt",
+        "exp": 100,
+        "coins": 50,
+        "progress": 1
+      },
+      {
+        "title": "Quest di Sam & Nick",
+        "desc": "Descrizione quest di Sam & Nick",
+        "exp": 25,
+        "coins": 100,
+        "progress": 3
+      },
+      {
+        "title": "Quest di Aster",
+        "desc": "Descrizione quest di Aster",
+        "exp": 50,
+        "coins": 25,
+        "progress": 1
+      },
+      {
+        "title": "Quest di Azza",
+        "desc": "Descrizione quest di Azza",
+        "exp": 100,
+        "coins": 150,
+        "progress": 1
+      },
+      {
+        "title": "Quest di Jessica",
+        "desc": "Descrizione quest di Jessica",
+        "exp": 100,
+        "coins": 100,
+        "progress": 1
+      }
+    ]
+
